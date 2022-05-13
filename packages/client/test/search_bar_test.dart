234b0d236cfc44a816ab49333b0b0db8101963e7
u20587059 <85702473+SengiSliko@users.noreply.github.com>
2022-05-13 05:35:05 +0200

    sarch bad widget test :construction

---
+++ packages/client/test/search_bar_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:client/search_bar.dart';
+
+void main() {
+ testWidgets("Testing if the navbar component works and is created",
+      (WidgetTester tester) async {
+    final SearchBar searchbar = const SearchBar();
+    
+
+    await tester.pumpWidget(MaterialApp(
+        key: Key('TestKey'), home: Scaffold( body: searchbar)));
+
+    
+    expect(find.byWidget(searchbar), findsOneWidget);
+  });
+}
