a4d15f6a9f844859fd3f0cba8c55f56c5337ca6d
u20587059 <85702473+SengiSliko@users.noreply.github.com>
2022-05-13 03:24:33 +0200

    search history function test file

---
+++ packages/client/test/search_historty_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:client/nav_bar.dart';
+import 'package:client/main.dart';
+import 'package:client/home.dart';
+import 'package:client/search_bar.dart';
+import 'package:client/searchhistory.dart';
+
+void main() {
+  test("search history function test check", () async {
+    var testlist = new List.empty();
+
+    //  var final = SearchHistory(testlist);
+    var historyList = <String>[];
+    
+  });
+}
