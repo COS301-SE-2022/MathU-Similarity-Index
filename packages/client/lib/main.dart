85bbc25e3a7b50b49b4dcfabd8a6c1759b94701c
mattreed01 <matthewreed0107@gmail.com>
2022-07-20 18:08:01 +0200

    💄 (UI): Created skeleton for new main.dart file

---
+++ packages/client/lib/main.dart
@@ -0,0 +1,21 @@
+//Imports
+
+/*
+NOTE:
+######################################################################
+The only thing to be added to this file is the routes used for getting
+from page to page.
+######################################################################
+*/
+
+//Actual code
+// void main() {
+//   runApp(MaterialApp(
+//     home: Home(history: history),
+//     routes: {
+//       '/home.dart': (context) => Home(history: history),
+//       '/searchhistory.dart': (context) => SearchHistory(history),
+//       '/viewAllProblems.dart': (context) => viewAll(),
+//     },
+//   ));
+// }
