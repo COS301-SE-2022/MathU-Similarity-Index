8dd7be3108a59f210abab951dfcf793255a7d74d
Tawanda Jimu <74461990+TAWANDA-CODER@users.noreply.github.com>
2022-07-26 16:43:57 +0200

    :sparkles: Added Mathematical Tools and Past Papers to navigation drawer

---
+++ packages/client/lib/NavigationDrawer.dart
@@ -0,0 +1,188 @@
+//Imports
+import 'package:flutter/material.dart';
+/*
+NOTE:
+################################################################################
+################################################################################
+*/
+
+//Code
+class NavigationDrawer extends StatefulWidget {
+  const NavigationDrawer({Key? key}) : super(key: key);
+
+  @override
+  State<NavigationDrawer> createState() => _NavigationDrawerState();
+}
+
+class _NavigationDrawerState extends State<NavigationDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: const Color(0xFF003255),
+      elevation: 1.0,
+      child: ListView(
+        children: [
+          ListTile(
+            mouseCursor: SystemMouseCursors.click,
+            contentPadding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+            onTap: goToPage(1),
+            title: Text(
+              'History',
+              style: TextStyle(color: Colors.grey[200], fontSize: 18.0),
+              textAlign: TextAlign.center,
+            ),
+            trailing: Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.grey[200],
+            ),
+            leading: Icon(
+              Icons.history,
+              color: Colors.grey[200],
+            ),
+          ),
+          const Divider(
+            height: 5.0,
+            color: Color(0xFFA23B80),
+          ),
+          ListTile(
+            mouseCursor: SystemMouseCursors.click,
+            contentPadding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+            onTap: goToPage(2),
+            title: Text(
+              'Saved',
+              style: TextStyle(color: Colors.grey[200], fontSize: 18.0),
+              textAlign: TextAlign.center,
+            ),
+            trailing: Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.grey[200],
+            ),
+            leading: Icon(
+              Icons.bookmark,
+              color: Colors.grey[200],
+            ),
+          ),
+          const Divider(
+            height: 5.0,
+            color: Color(0xFFA23B80),
+          ),
+          ListTile(
+            mouseCursor: SystemMouseCursors.click,
+            contentPadding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+            onTap: goToPage(3),
+            title: Text(
+              'Mathematical Tools',
+              style: TextStyle(color: Colors.grey[200], fontSize: 18.0),
+              textAlign: TextAlign.center,
+            ),
+            trailing: Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.grey[200],
+            ),
+            leading: Icon(
+              Icons.calculate_outlined,
+              color: Colors.grey[200],
+            ),
+          ),
+          const Divider(
+            height: 5.0,
+            color: Color(0xFFA23B80),
+          ),
+          ListTile(
+            mouseCursor: SystemMouseCursors.click,
+            contentPadding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+            onTap: goToPage(4),
+            title: Text(
+              'Chapters',
+              style: TextStyle(color: Colors.grey[200], fontSize: 18.0),
+              textAlign: TextAlign.center,
+            ),
+            trailing: Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.grey[200],
+            ),
+            leading: Icon(
+              Icons.book,
+              color: Colors.grey[200],
+            ),
+          ),
+          const Divider(
+            height: 5.0,
+            color: Color(0xFFA23B80),
+          ),
+          ListTile(
+            mouseCursor: SystemMouseCursors.click,
+            contentPadding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+            onTap: goToPage(5),
+            title: Text(
+              'Past Papers',
+              style: TextStyle(color: Colors.grey[200], fontSize: 18.0),
+              textAlign: TextAlign.center,
+            ),
+            trailing: Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.grey[200],
+            ),
+            leading: Icon(
+              Icons.archive_outlined,
+              color: Colors.grey[200],
+            ),
+          ),
+          const Divider(
+            height: 5.0,
+            color: Color(0xFFA23B80),
+          ),
+          ListTile(
+            mouseCursor: SystemMouseCursors.click,
+            contentPadding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+            onTap: goToPage(6),
+            title: Text(
+              'Settings',
+              style: TextStyle(color: Colors.grey[200], fontSize: 18.0),
+              textAlign: TextAlign.center,
+            ),
+            trailing: Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.grey[200],
+            ),
+            leading: Icon(
+              Icons.settings,
+              color: Colors.grey[200],
+            ),
+          ),
+          const Divider(
+            height: 5.0,
+            color: Color(0xFFA23B80),
+          ),
+        ],
+      ),
+    );
+  }
+
+  //Methods
+  goToPage(int pageNumber) {
+    switch (pageNumber) {
+      case 1:
+        print("Go To History");
+        break;
+      case 2:
+        print("Go To Saved");
+        break;
+      case 3:
+        print("Go To Mathematical Tools");
+        break;
+      case 4:
+        print("Go To Chapters");
+        break;
+      case 5:
+        print("Go To Past Papers");
+        break;
+      case 6:
+        print("Go To Settings");
+        break;
+      default:
+        print("Here");
+        break;
+    }
+  }
+}
---
+++ packages/client/lib/titlebar.dart
@@ -0,0 +1,51 @@
+//Imports
+import 'package:flutter/material.dart';
+
+/*
+NOTE
+################################################################################
+################################################################################
+*/
+
+//Code
+class TitleBar extends StatelessWidget implements PreferredSizeWidget {
+  const TitleBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      centerTitle: true,
+      title: RichText(
+        text: const TextSpan(children: <TextSpan>[
+          TextSpan(
+              text: 'Math',
+              style: TextStyle(color: Colors.white, fontSize: 36.0)),
+          TextSpan(
+              text: 'U ',
+              style: TextStyle(
+                  color: Colors.pink,
+                  fontSize: 36.0,
+                  fontWeight: FontWeight.bold)),
+          TextSpan(
+              text: 'Similarity Index',
+              style: TextStyle(color: Colors.white, fontSize: 36.0)),
+        ]),
+      ),
+      backgroundColor: const Color(0xFF003255),
+    );
+    // return AppBar(
+    //   title: const Text(
+    //     'MathU Similarity Index',
+    //     style: TextStyle(
+    //       fontSize: 36.0,
+    //     ),
+    //   ),
+    //   centerTitle: true,
+    //   elevation: 0.0,
+    //   backgroundColor: Color(0xFF003255),
+    // );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(66);
+}
