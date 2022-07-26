244439b4f60ffea412f114e7352a9a16a624f525
Matthew Ronald Reed <u19100133@tuks.co.za>
2022-07-26 13:57:44 +0200

    💄(UI): Added widget with helpful display text

---
+++ packages/client/lib/noResultsText.dart
@@ -0,0 +1,29 @@
+//Imports
+import 'package:flutter/material.dart';
+
+/*
+NOTE
+################################################################################
+This widget is used to display a helpful message indicating whether search 
+results returned successfully
+################################################################################
+*/
+
+//Code
+class NothingToSeeHere extends StatelessWidget {
+  const NothingToSeeHere({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        'There is nothing to see here...',
+        style: TextStyle(
+          color: Colors.grey[700],
+          letterSpacing: 1.2,
+          wordSpacing: 2.0,
+        ),
+      ),
+    );
+  }
+}
