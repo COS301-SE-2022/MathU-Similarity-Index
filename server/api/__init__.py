8e0c846a26f89965927093d0d542359a3070a484
DamianJordaan <u20473509@tuks.co.za>
2022-07-27 14:11:04 +0200

    🚚🐛 (Backend) Updated Server routes and imports

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask
 
+from server.services.testingpathend import testthings
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello!'
+    return 'MathU Similarity Index Server'
