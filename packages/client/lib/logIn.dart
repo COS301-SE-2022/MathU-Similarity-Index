9f429685af4abe6a10edb8dcdc7495167858a4a9
Matthew Ronald Reed <u19100133@tuks.co.za>
2022-07-27 13:32:20 +0200

    💄 (UI): Updating Log In screen

---
+++ packages/client/lib/logIn.dart
@@ -0,0 +1,26 @@
+//Imports
+import 'package:flutter/material.dart';
+import 'package:client/titlebar.dart';
+import 'package:client/NavigationDrawer.dart';
+import 'package:client/apiInterface.dart';
+
+/*
+NOTE
+################################################################################
+################################################################################
+*/
+
+//Code
+class LogIn extends StatefulWidget {
+  const LogIn({Key? key}) : super(key: key);
+
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
