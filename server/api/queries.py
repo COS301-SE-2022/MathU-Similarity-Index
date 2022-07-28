4230e34c6cc49d922ef0325d239588cea6a6382d
DamianJordaan <u20473509@tuks.co.za>
2022-07-29 01:38:48 +0200

    🤡(API) Added mock returns for updated schema

--- server/api/queries.py
+++ server/api/queries.py
@@ -111,14 +111,137 @@ def resolve_search(obj, info, input, islogedin, useremail, apikey):
     return payload
 
 def get_user_history(obj, info, useremail, apikey):
-    pass
+    payload = [{
+            "id": 3,
+            "mathml": f'<math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mrow><mn>1</mn><mo>+</mo><mn>2</mn></mrow></math>',
+            "latex": "1+2",
+            "type": "Equation",
+            "category": "Addition"
+        },
+        {
+            "id": 2,
+            "mathml": f'<math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mrow><mn>3</mn><mo>-</mo><mn>2</mn></mrow></math>',
+            "latex": "3-2",
+            "type": "Equation",
+            "category": "Subtraction"
+        }]
+    return payload
 
-def GetAllComments(obj, info, ):
-    pass
-def GetComments(obj, info, problemid):
-    pass
-def GetFavoriteProblems(obj, info, useremail):
-    pass
+def get_all_comments(obj, info, ):
+    payload = [
+        {
+            "problemid": 0,
+            "datetiem": {
+                "year": 0,
+                "month": 0,
+                "day": 0,
+                "hour": 0,
+                "minute": 0,
+                "second": 0,
+                "timezone": "",
+            },
+            "useremail": "test@email.com",
+            "comment": "test comment"
+        },
+        {
+            "problemid": 0,
+            "datetiem": {
+                "year": 0,
+                "month": 0,
+                "day": 0,
+                "hour": 0,
+                "minute": 0,
+                "second": 1,
+                "timezone": "",
+            },
+            "useremail": "test@email.com",
+            "comment": "test comment 2"
+        },
+        {
+            "problemid": 1,
+            "datetiem": {
+                "year": 0,
+                "month": 0,
+                "day": 0,
+                "hour": 0,
+                "minute": 0,
+                "second": 1,
+                "timezone": "",
+            },
+            "useremail": "test@email.com",
+            "comment": "test comment 3"
+        }
+    ]
+    return payload
 
-def AuthenticateLogin(obj, info, useremail, passwordsalt):
-    pass
+def get_comments(obj, info, problemid):
+    payload = [
+        {
+            "problemid": 0,
+            "datetiem": {
+                "year": 0,
+                "month": 0,
+                "day": 0,
+                "hour": 0,
+                "minute": 0,
+                "second": 0,
+                "timezone": "",
+            },
+            "useremail": "test@email.com",
+            "comment": "test comment"
+        },
+        {
+            "problemid": 0,
+            "datetiem": {
+                "year": 0,
+                "month": 0,
+                "day": 0,
+                "hour": 0,
+                "minute": 0,
+                "second": 1,
+                "timezone": "",
+            },
+            "useremail": "test@email.com",
+            "comment": "test comment 2"
+        }
+    ]
+    return payload
+
+def get_favorite_problems(obj, info, useremail):
+    payload = [
+        {
+            "id": 3,
+            "mathml": f'<math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mrow><mn>1</mn><mo>+</mo><mn>2</mn></mrow></math>',
+            "latex": "1+2",
+            "type": "Equation",
+            "category": "Addition"
+        },
+        {
+            "id": 2,
+            "mathml": f'<math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mrow><mn>3</mn><mo>-</mo><mn>2</mn></mrow></math>',
+            "latex": "3-2",
+            "type": "Equation",
+            "category": "Subtraction"
+        }
+    ]
+    return payload
+
+def authenticate_login(obj, info, useremail, passwordsalt):
+    payload = {
+        "success": True,
+        "msg": "Login Success",
+        "user": {
+            "useremail": "test@email.com",
+            "username": "none",
+            "apikey": "testkey",
+            "isadmin": False,
+            "darktheme": False
+        }
+    }
+    return payload
+
+def get_server_settings(obj, info, useremail, apikey):
+    payload = {
+        "autocaching": True
+    }
+    return payload
