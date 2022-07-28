e2eab1b3541e7bdf37e9aba3b3ef0e702b111c00
Matthew Ronald Reed <u19100133@tuks.co.za>
2022-07-28 18:00:30 +0200

    💄 (UI): Updated UserData class

--- packages/client/lib/UserData.dart
+++ packages/client/lib/UserData.dart
@@ -1,4 +1,5 @@
 //Imports
+import 'dart:convert';
 
 /*
 NOTE
@@ -17,14 +18,44 @@ class UserData {
 
   UserData._internal();
 
-  String _userID = '';
+  String _userID = 'default';
+  String _apiKey = 'default';
+  bool _isLoggedIn = false;
+  bool _isAdmin = false;
+  String _settings = jsonEncode({
+    'darkMode': false,
+  });
 
   void setUserID(String uid) {
     _userID = uid;
   }
 
+  void setAPIKey(String apke) {
+    _apiKey = apke;
+  }
+
+  void setLoggedIn(bool b) {
+    _isLoggedIn = b;
+  }
+
+  void setAdmin(bool b) {
+    _isAdmin = b;
+  }
+
   getUserID() {
     return _userData;
+  }
+
+  getAPIKey() {
+    return _apiKey;
+  }
+
+  getLogStatus() {
+    return _isLoggedIn;
+  }
+
+  getAdminStatus() {
+    return _isAdmin;
   }
 }
 
