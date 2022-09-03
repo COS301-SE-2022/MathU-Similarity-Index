17152832ae83a771fce255b9be9da946817183cd
Tawanda Jimu <74461990+TAWANDA-CODER@users.noreply.github.com>
2022-09-03 15:05:32 +0200

    :sparkles: :lipstick: Mathematical Tools Calculator Function Keys
    Mathematical Tools Calculator Function Keys

---
+++ packages/client/lib/Mathematical Tools/Calculator/functionkeys.dart
@@ -0,0 +1,307 @@
+//Imports
+import 'package:flutter/material.dart';
+
+/*
+NOTE
+################################################################################
+################################################################################
+*/
+
+//Code
+class Functionkeys extends StatefulWidget {
+  const Functionkeys({Key? key}) : super(key: key);
+
+  @override
+  State<Functionkeys> createState() => _FunctionkeysState();
+}
+
+class _FunctionkeysState extends State<Functionkeys> {
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      flex: 9,
+      child: Row(
+        children: <Widget>[
+          Flexible(
+              child: Column(
+            children: const <Widget>[
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+              Flexible(
+                  child: ElevatedButton(
+                onPressed: null,
+                child: Text(')', style: TextStyle(fontSize: 10)),
+              )),
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+              Flexible(
+                  child: ElevatedButton(
+                onPressed: null,
+                child: Text('(', style: TextStyle(fontSize: 10)),
+              )),
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+              Flexible(
+                  child: ElevatedButton(
+                onPressed: null,
+                child: Text('(', style: TextStyle(fontSize: 10)),
+              )),
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+              Flexible(
+                  child: ElevatedButton(
+                onPressed: null,
+                child: Text('(', style: TextStyle(fontSize: 10)),
+              )),
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+            ],
+          )),
+          const SizedBox(
+            height: 3,
+            width: 2,
+          ),
+          Flexible(
+              child: Column(
+            children: const <Widget>[
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+              Flexible(
+                  child: ElevatedButton(
+                onPressed: null,
+                child: Text(')', style: TextStyle(fontSize: 10)),
+              )),
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+              Flexible(
+                  child: ElevatedButton(
+                onPressed: null,
+                child: Text('(', style: TextStyle(fontSize: 10)),
+              )),
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+              Flexible(
+                  child: ElevatedButton(
+                onPressed: null,
+                child: Text('(', style: TextStyle(fontSize: 10)),
+              )),
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+              Flexible(
+                  child: ElevatedButton(
+                onPressed: null,
+                child: Text('(', style: TextStyle(fontSize: 10)),
+              )),
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+            ],
+          )),
+          const SizedBox(
+            height: 3,
+            width: 2,
+          ),
+          Flexible(
+              child: Column(
+            children: const <Widget>[
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+              Flexible(
+                  child: ElevatedButton(
+                onPressed: null,
+                child: Text('(', style: TextStyle(fontSize: 10)),
+              )),
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+              Flexible(
+                  child: ElevatedButton(
+                onPressed: null,
+                child: Text('(', style: TextStyle(fontSize: 10)),
+              )),
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+              Flexible(
+                  child: ElevatedButton(
+                onPressed: null,
+                child: Text('(', style: TextStyle(fontSize: 10)),
+              )),
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+              Flexible(
+                  child: ElevatedButton(
+                onPressed: null,
+                child: Text('(', style: TextStyle(fontSize: 10)),
+              )),
+            ],
+          )),
+          const SizedBox(
+            height: 3,
+            width: 2,
+          ),
+          Flexible(
+              child: Column(
+            children: const <Widget>[
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+              Flexible(
+                  child: ElevatedButton(
+                onPressed: null,
+                child: Text('(', style: TextStyle(fontSize: 10)),
+              )),
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+              Flexible(
+                  child: ElevatedButton(
+                onPressed: null,
+                child: Text('(', style: TextStyle(fontSize: 10)),
+              )),
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+              Flexible(
+                  child: ElevatedButton(
+                onPressed: null,
+                child: Text('(', style: TextStyle(fontSize: 10)),
+              )),
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+              Flexible(
+                  child: ElevatedButton(
+                onPressed: null,
+                child: Text('(', style: TextStyle(fontSize: 10)),
+              )),
+            ],
+          )),
+          const SizedBox(
+            height: 3,
+            width: 2,
+          ),
+          Flexible(
+              child: Column(
+            children: const <Widget>[
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+              Flexible(
+                  child: ElevatedButton(
+                onPressed: null,
+                child: Text('(', style: TextStyle(fontSize: 10)),
+              )),
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+              Flexible(
+                  child: ElevatedButton(
+                onPressed: null,
+                child: Text('(', style: TextStyle(fontSize: 12)),
+              )),
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+              Flexible(
+                  child: ElevatedButton(
+                onPressed: null,
+                child: Text('(', style: TextStyle(fontSize: 12)),
+              )),
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+              Flexible(
+                  child: ElevatedButton(
+                onPressed: null,
+                child: Text('(', style: TextStyle(fontSize: 12)),
+              )),
+            ],
+          )),
+          const SizedBox(
+            height: 3,
+            width: 2,
+          ),
+          Flexible(
+              child: Column(
+            children: const <Widget>[
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+              Flexible(
+                  child: ElevatedButton(
+                onPressed: null,
+                child: Text('(', style: TextStyle(fontSize: 12)),
+              )),
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+              Flexible(
+                  child: ElevatedButton(
+                onPressed: null,
+                child: Text('(', style: TextStyle(fontSize: 12)),
+              )),
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+              Flexible(
+                  child: ElevatedButton(
+                onPressed: null,
+                child: Text('(', style: TextStyle(fontSize: 12)),
+              )),
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+              Flexible(
+                  child: ElevatedButton(
+                onPressed: null,
+                child: Text('(', style: TextStyle(fontSize: 12)),
+              )),
+              SizedBox(
+                height: 3,
+                width: 2,
+              ),
+            ],
+          )),
+        ],
+      ),
+    );
+  }
+}
