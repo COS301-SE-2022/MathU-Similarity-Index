144fc68a94050a9f90ed8450b2550826d9e161dc
Tawanda Jimu <74461990+TAWANDA-CODER@users.noreply.github.com>
2022-09-03 15:02:19 +0200

    :sparkles: :lipstick:  Mathematical Tools Calculator Screenr
    New Screen for Calculator on Calculations Page

---
+++ packages/client/lib/Mathematical Tools/Calculator/calculatorscreen.dart
@@ -0,0 +1,47 @@
+//Imports
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+import 'package:new_ui/Calculator/bottomscreen.dart';
+import 'package:new_ui/Calculator/topscreen.dart';
+
+/*
+NOTE
+################################################################################
+################################################################################
+*/
+
+//Code
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CalculatorScreen> createState() => _CalculatorScreenState();
+}
+
+const Color Calculator_background = Color(0xFF1D3557);
+const Color Calculator_background_1 = Color(0xFF0476c7);
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+        flex: 5,
+        child: Container(
+            height: 3000,
+            decoration: const BoxDecoration(
+              color: Colors.pink,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(36),
+                topRight: Radius.circular(36),
+              ),
+            ),
+            child: Column(
+              children: <Widget>[
+                const Flexible(child: TopScreen()),
+                Flexible(child: Container()),
+                const Flexible(child: BottomScreen()),
+              ],
+            )));
+  }
+}
