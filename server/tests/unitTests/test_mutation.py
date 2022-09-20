cd3a452eabb95a3e547d9c0904288451bf205033
Risenga (RT)  Sono <85702473+SengiSliko@users.noreply.github.com>
2022-09-20 21:49:31 +0200

    :white_check_mark: updated tests

--- server/tests/unitTests/test_mutation.py
+++ server/tests/unitTests/test_mutation.py
@@ -6,17 +6,6 @@ from server.api.mutation import *
 
 class TestMutations:
 
-  payload = {
-        "success": True,
-        "msg": "",
-        "equation": {
-            "id": 0,
-            "mathml": "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mrow><mn>1</mn><mo>+</mo><mn>2</mn></mrow></math>",
-            "latex": "1+2",
-            "type": "Equation",
-            "category": "Addition"
-        }
-  }
 
   def test_add_equation(self):
     payload = {
@@ -40,10 +29,22 @@ class TestMutations:
     assert resolve_add_user_search_click("","","","","") == True
 
   def test_user_sign_up(self):
-    assert resolve_user_sign_up("","","","") == True
+   payload = {
+        "success": True,
+        "msg": "signup Success",
+        "user": {
+            "useremail": "test@email.com",
+            "username": "none",
+            "apikey": "testkey",
+            "isadmin": False,
+            "darktheme": False
+        }
+    }
+
+   assert resolve_user_sign_up("","","","") == payload
 
   def test_auto_caching(self):
-    assert resolve_set_auto_caching("","","","") == True
+    assert resolve_set_auto_caching("","","","","") == True
 
   def test_set_theme(self):
     assert resolve_set_theme("","","","","") == True
