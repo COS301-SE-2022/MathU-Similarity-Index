ec6e9eafcf569e6487a74ef8e4fb208f2cd16553
Risenga (RT)  Sono <85702473+SengiSliko@users.noreply.github.com>
2022-09-22 14:07:54 +0200

    :hammer: updated query

--- server/api/queries/resolve_area_of_circle.py
+++ server/api/queries/resolve_area_of_circle.py
@@ -1,13 +1,15 @@
 from flask import Flask, request, jsonify
 import math
 app = Flask(__name__)
-@app.route('/api/areacircle/',methods = ['GET'])
+@app.route('/api/2/', methods=['GET'])
 def areacircle():
-	try:
-		text_input = str(request.args['query'])
-		r = int(text_input)
-		Area = math.pi * r * r
-		Answer = "Area of the circle: " + str(round(Area,3))
-		return Answer
-	except Exception as e:
-		return "INVALID INPUT"
+    try:
+        text_input = str(request.args['query'])
+        r = int(text_input)
+        Area = math.pi * r * r
+        Answer = str(round(Area, 3))
+        Formula = 'π' + str(r) + f'\N{SUPERSCRIPT TWO}'
+
+        return Formula+"separator"+Answer
+    except Exception as e:
+        return "INVALID INPUT"
