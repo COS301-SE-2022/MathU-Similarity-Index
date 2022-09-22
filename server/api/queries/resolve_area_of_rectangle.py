7212fe8812c382bd7cec1693824f74183fd7dc50
Risenga (RT)  Sono <85702473+SengiSliko@users.noreply.github.com>
2022-09-22 14:14:23 +0200

    :hammer: updated query

--- server/api/queries/resolve_area_of_rectangle.py
+++ server/api/queries/resolve_area_of_rectangle.py
@@ -1,19 +1,21 @@
 
 from flask import Flask, request, jsonify
 app = Flask(__name__)
-@app.route('/api/arearectangle/',methods = ['GET'])
+@app.route('/api/4/', methods=['GET'])
 def arearectangle():
-	text_input = str(request.args['query'])
-	try:
-		abc = text_input.split(",")
-		abc = list(map(int, abc))
-		length = abc[0]
-		width = abc[1]
-		Area = length * width
-		Answer = "Area of the rectangle: " + str(Area)
-		return Answer
-	except Exception as e:
-		return "INVALID INPUT"
+    text_input = str(request.args['query'])
+    try:
+        abc = text_input.split(",")
+        abc = list(map(int, abc))
+        length = abc[0]
+        width = abc[1]
+        Area = length * width
+        Answer = str(Area)
+        Formula = str(length) + "×" + str(width);
+        return Formula+"separator"+Answer
+    except Exception as e:
+        return "INVALID INPUT"
+
 
 if __name__ == '__main__':
 	app.run()
