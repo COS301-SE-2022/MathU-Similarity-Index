aeb43ea78cb0f1bcd6767d99476a54c4d76c29fd
Risenga (RT)  Sono <85702473+SengiSliko@users.noreply.github.com>
2022-09-23 00:30:10 +0200

    :hammer: updated query

--- server/api/queries/resolve_mean.py
+++ server/api/queries/resolve_mean.py
@@ -1,11 +1,19 @@
+from flask import Flask, request, jsonify
+import matplotlib.pyplot as plt
+import numpy as np
+import sympy as sym
+import cmath
+import math
 import statistics
+
+app = Flask(__name__)
+
+@app.route('/api/13/', methods=['GET'])
 def mean():
-	input = "1,2,3,4,5,6,7,8"
-	text_input = str(input)
-	abc = text_input.split(",")
-	abc = list(map(int, abc))
-	print(statistics.mean(abc))
-
-mean()
-
-	
+    text_input = str(request.args['query'])
+    try:
+        abc = text_input.split(".")
+        abc = list(map(int, abc))
+        return str(statistics.mean(abc))
+    except Exception as e:
+        return "INVALID INPUT"
