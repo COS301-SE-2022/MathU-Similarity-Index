6c360b62b1d07f1d5aa702cc2527cd4be3bceeb1
Matthew Ronald Reed <u19100133@tuks.co.za>
2022-09-27 14:40:10 +0200

    💄 ✨ (UI): Created a progress spinner class

---
+++ packages/client/lib/load_icon.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+Widget LoadIcon() {
+  return Center(
+    child: SpinKitFoldingCube(
+      color: Color(0xFFEC5666),
+      size: 50.0,
+    ),
+  );
+}
