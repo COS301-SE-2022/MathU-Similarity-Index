d5f867c2c889973b54ade9fdc95567f003df1818
DamianJordaan <u20473509@tuks.co.za>
2022-09-30 01:24:04 +0200

    ✨ (Backend) updated test function file

--- server/testFunctions.py
+++ server/testFunctions.py
@@ -1,6 +1,6 @@
 print("start:")
 
-from db.handlers.problems import get_all_problems_favorite
+from db.handlers.problems import get_all_problems_favorite, get_all_problems_favorite_autocache
 
 res = get_all_problems_favorite("bob@email.com",[]) #problem_id, problem, user_search, has_memo, favorite
 
@@ -15,3 +15,13 @@ print("len: ",len(res_t))
 
 res_l = add_problem_links(res_t, 0)
 print("len: ",len(res_l))
+
+res = get_all_problems_favorite_autocache(-1,"bob@email.com",[2,3]) #problem_id, problem, user_search, has_memo, favorite, similarity
+
+print("len: ",len(res))
+print(res)
+
+from db.handlers.problems import get_problem_data
+res = get_problem_data(4)
+print("len: ",len(res))
+print(res)
