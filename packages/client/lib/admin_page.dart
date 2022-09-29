43aec8a6c9848d958201ab53df41e23662e9ff13
Matthew Ronald Reed <u19100133@tuks.co.za>
2022-09-29 23:41:59 +0200

    💄 (UI): Added a new button to admin console

--- packages/client/lib/admin_page.dart
+++ packages/client/lib/admin_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:client/apiInterface.dart';
 import 'package:client/titlebar.dart';
 import 'package:client/NavigationDrawer.dart';
+import 'package:client/load_icon.dart';
 
 /*
 NOTE
@@ -22,6 +23,25 @@ class AdminPage extends StatefulWidget {
 
 class _AdminPageState extends State<AdminPage> {
   bool autoCaching = false;
+  bool isLoading = false;
+
+  void checkServerSettings() async {
+    setState(() {
+      isLoading = true;
+    });
+    dynamic temp = await apiObj.getServerSettings();
+    setState(() {
+      isLoading = false;
+      autoCaching = temp['autocaching'];
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    checkServerSettings();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +53,10 @@ class _AdminPageState extends State<AdminPage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              Visibility(
+                visible: isLoading,
+                child: LoadIcon(),
+              ),
               SizedBox(
                 width: 800,
                 child: Card(
@@ -50,6 +74,47 @@ class _AdminPageState extends State<AdminPage> {
                           inactiveTrackColor: Color(0xFF003255),
                           onChanged: (val) {
                             setState(() => autoCaching = val);
+                          },
+                        ),
+                        ElevatedButton(
+                          style: ButtonStyle(
+                            elevation:
+                                MaterialStateProperty.resolveWith<double>(
+                                    ((states) => 0)),
+                            backgroundColor:
+                                MaterialStateProperty.resolveWith(getColor),
+                          ),
+                          child: Text('Confirm'),
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) => AlertDialog(
+                                title: const Text('Making Changes?'),
+                                content: SingleChildScrollView(
+                                  child: ListBody(
+                                    children: [
+                                      Text(
+                                          'We need your password to approve this action'),
+                                      TextFormField(
+                                        obscureText: true,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () =>
+                                        Navigator.pop(context, 'Cancel'),
+                                    child: const Text('Cancel'),
+                                  ),
+                                  TextButton(
+                                    onPressed: () =>
+                                        Navigator.pop(context, 'OK'),
+                                    child: const Text('OK'),
+                                  ),
+                                ],
+                              ),
+                            );
                           },
                         ),
                       ],
@@ -83,7 +148,7 @@ class _AdminPageState extends State<AdminPage> {
                             backgroundColor:
                                 MaterialStateProperty.resolveWith(getColor),
                           ),
-                          child: Text('Authenticate'),
+                          child: Text('Confirm'),
                           onPressed: () {
                             showDialog(
                               context: context,
@@ -147,7 +212,7 @@ class _AdminPageState extends State<AdminPage> {
                             backgroundColor:
                                 MaterialStateProperty.resolveWith(getColor),
                           ),
-                          child: Text('Authenticate'),
+                          child: Text('Confirm'),
                           onPressed: () {
                             showDialog(
                               context: context,
@@ -190,6 +255,22 @@ class _AdminPageState extends State<AdminPage> {
         ),
       ),
     );
+  }
+
+  void setAutoCaching(String pw) async {
+    dynamic temp = await apiObj.setServerSettings(autoCaching, pw);
+
+    checkServerSettings();
+
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: (temp['success'])
+          ? Text('Change Recorded')
+          : Text('Woops, Something went wrong...'),
+      width: 400,
+      behavior: SnackBarBehavior.floating,
+      duration: const Duration(milliseconds: 1500),
+      padding: EdgeInsets.all(10),
+    ));
   }
 
   Color getColor(Set<MaterialState> states) {
